79fc2b317e3c0722f19409ff022586da9035f622
macymcbee <macy.mcbee13@gmail.com>
2025-05-01 15:04:33 -0700

    Adding last page

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ window.addEventListener('scroll', () => {
   
     // When she reaches the right side of the screen
     if (moveRight + 100 >= window.innerWidth) {
+      window.location.href = "page5.html"; // Redirect to a new page
+    }
+  });
+
+
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const alice = document.getElementById('alice');
+    
+    // Move Alice to the right based on scroll
+    let moveRight = scrollTop * 1.5; // Adjust multiplier to match screen width
+    alice.style.left = `${moveRight}px`;
+  
+
+    if (moveRight + 50 >= window.innerWidth) {
       window.location.href = "index.html"; // Redirect to a new page
     }
   });
